79175bcf9323fbc9433338d86e85e077d6a33c1c
Vaaruni2797 <vaaruni.desai@gmail.com>
2024-10-16 22:03:27 -0700

    UI changes and chart addition

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 import pandas as pd
+import plotly.graph_objs as go
+import plotly.io as pio
+from plotly.subplots import make_subplots
 from flask import Flask, request, render_template
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
@@ -25,8 +28,18 @@ def predict_datapoint():
         pred_df = data.get_data_as_data_frame()
         predict_pipeline = PredictPipeline()
         results, probabilities = predict_pipeline.predict(pred_df['summary'])
+        prob_dict = {}
+        for prob, genre in probabilities:
+            prob_dict[genre]=prob
 
-        return render_template('home.html', results = results[0], probabilities=probabilities)
+        fig = go.Figure([go.Bar(x=list(prob_dict.values()), y=list(prob_dict.keys()),orientation='h')])
+        fig.update_layout(title='Predicted Genre Probabilities', 
+                        xaxis_title='Genre', 
+                        yaxis_title='Probability',
+                        yaxis=dict(autorange='reversed'))
+        chart_html = pio.to_html(fig, full_html=False)
+
+        return render_template('home.html', results=results[0], chart=chart_html)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
